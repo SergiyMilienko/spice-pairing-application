70b2b36ee5afa310faeffe00c44382ff53fb138c
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-11-22 13:48:26 +0200

    language switch dropdown menu

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, {useState} from 'react'
 import "./style.css"
 
 const Navbar = () => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const handleDropdownToggle = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };  
+
   return (
     <nav className="nav">
         <div className="container">
@@ -14,11 +21,17 @@ const Navbar = () => {
                     <li className="nav-list__item">
                         <a href="#faqs" className="nav-list__link">FAQs</a>
                     </li>
-                    <li className="nav-list__item">
+                    <li className="nav-list__item" onClick={handleDropdownToggle}>
                         <a href="#language" className="nav-list__link">Language</a>
                         <button itemID="bottom-arrow">
                             <img className="choose" src="./img/right-arrow.svg" alt=""/>
                         </button>
+                        {isDropdownOpen && (
+                        <ul className="dropdown-menu languages">
+                            <li> <img src="./img/english.jpg" alt=""/>English</li>
+                            <li> <img src="./img/ukrainian.jpg" alt=""/>Ukrainian</li>
+                        </ul>
+                    )}
                     </li>
                 </ul>
             </div>
