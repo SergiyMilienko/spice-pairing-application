d81db38bdd4551cfb1b876cb4250671badf4d5f6
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-11-23 16:52:21 +0200

    translation for accordions

--- src/components/Questions/index.jsx
+++ src/components/Questions/index.jsx
@@ -1,16 +1,18 @@
 import React, {useState} from 'react'
 import AccordionItem from '../AccordionItem'
 import './style.css';
+import { useTranslation } from 'react-i18next';
 
 const Questions = () => {
 
+    const { t } = useTranslation();
     const accordionData = [
-        { title: 'How does the spice pairing feature work?', content: "Our spice pairing feature analyzes flavor profiles, aromas, and traditional pairings to recommend complementary spices for your dishes." },
-        { title: 'Can I customize or input my own spices?', content: "Currently, our app doesn't support custom spices. However, we're exploring this feature for future updates." },
-        { title: 'What are the criteria for determining good, bad, or neutral spice pairings?', content: "Pairings are based on flavor affinities, regional traditions, and culinary expertise, ensuring a balanced and delicious combination." },
-        { title: 'Is the spice pairing based on specific cuisines or dishes?', content: "Yes, our spice pairing considers the characteristics of various cuisines and provides tailored recommendations for specific dishes." },
-        { title: 'Is the app available in multiple languages?', content: "Yes, our app supports multiple languages. You can easily switch between languages in the settings." },
-        { title: 'Do you have nutritional information for the spices?', content: "Currently, our app focuses on flavor pairing. Nutritional information may be considered for future updates." },
+        { title: t('questions.question-1'), content: t('answers.answer-1') },
+        { title: t('questions.question-2'), content: t('answers.answer-2') },
+        { title: t('questions.question-3'), content: t('answers.answer-3') },
+        { title: t('questions.question-4'), content: t('answers.answer-4') },
+        { title: t('questions.question-5'), content: t('answers.answer-5') },
+        { title: t('questions.question-6'), content: t('answers.answer-6') },
       ];
     
   const [openItemIndex, setOpenItemIndex] = useState(null);
